4c89fa3219acc0f5cbcedac0f96e98ccbef95c2a
Elisha <elisha.sarkis96@gmail.com>
2018-02-24 17:26:57 -0500

    Build out group logic

--- Assets/_Scripts/Boid.cs
+++ Assets/_Scripts/Boid.cs
@@ -5,14 +5,78 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour 
 {
-	public float _moveSpeed;
+	public float moveSpeed;
+	public float rotationSpeed;
+
+	public float avoidProximity = 1.0f;
+
+	Vector3 averageHeadingOfGroup;
+	Vector3 averagePositionOfGroup;
+
+	// Any Boid objects further than this will be ignored in calculations
+	float minDistanceToNeighbour;
 
 	Vector3 _separation;	
 	Vector3 _alignment;
 
+	/// Reference to BoidsController of this Boid.
+	public BoidsController _parentBoidsController;
 
 	void Update () 
 	{
-		transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+		// One in 5 chance of readjusting this frame
+		if (Random.Range(0, 5) < 1)
+		{
+			UpdateTrajectory();
+		}
+		
+		//transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		transform.Translate(0, 0, Time.deltaTime * moveSpeed);
+
+	}
+
+	void UpdateTrajectory()
+	{
+		Vector3 avoidance 	= Vector3.zero; // Avoidance vector for collision intelligence.
+		Vector3 groupCenter	= Vector3.zero; // The center of the group
+		float groupSpeed = 0.1f; // TEMP - Do I need this here?
+		int groupSize = 0; // TEMP - total number of neighbours in our group
+
+		// Iterating through all Boid GameObjects
+		_parentBoidsController.allBoids.Remove(gameObject); // Remove self from List before iteration
+		foreach (GameObject boidObject in _parentBoidsController.allBoids)
+		{
+			float distanceToCurrentNeighbour = Vector3.Distance(boidObject.transform.position, transform.position);
+			if (distanceToCurrentNeighbour <= minDistanceToNeighbour)
+			{
+				groupCenter += boidObject.transform.position; // Average out center based on nearby relevant neighbour position
+				groupSize++;
+
+				if (distanceToCurrentNeighbour < avoidProximity)
+				{
+					// Account for proximity
+					avoidance = avoidance + (transform.position - boidObject.transform.position);
+				}
+
+				// TEMP - what does this do?
+				Boid otherBoid = boidObject.GetComponent<Boid>();
+				groupSpeed = groupSpeed + otherBoid.moveSpeed;
+			}
+		}
+		_parentBoidsController.allBoids.Remove(gameObject); // add self back to list
+
+		if (groupSize > 0)
+		{
+			groupCenter = (groupCenter / groupSize) + (_parentBoidsController.GetGoalPosition() - transform.position);
+			moveSpeed += groupSpeed / groupSize;
+
+			Vector3 direction = (groupCenter + avoidance) - transform.position;
+			if (direction != Vector3.zero)
+			{
+				transform.rotation = Quaternion.Slerp(transform.rotation,
+													Quaternion.LookRotation(direction),
+													rotationSpeed * Time.deltaTime);
+			}
+		}
 	}
 }
--- Assets/_Scripts/BoidsController.cs
+++ Assets/_Scripts/BoidsController.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class BoidsController : MonoBehaviour 
 {
-	/// The prefab GameObject
-	public GameObject _prefabBoid;
-	/// Total number of boids belonging to this BoidsController
-	public int _numBoids;
-	/// List of all GameObjects managed by this BoidsController
-	public List<GameObject> _allBoids = new List<GameObject>();
-	/// Max spawn distance from this BoidsController transform position
-	public int _spawnRadius;
-	/// Reference to in-scene goal GameObject for Boid objects in this BoidsController
-	public GameObject _goalObject;
+	/// The prefab GameObject.
+	public GameObject prefabBoid;
+	/// Total number of boids belonging to this BoidsController.
+	public int numBoids;
+	/// List of all GameObjects managed by this BoidsController.
+	public List<GameObject> allBoids = new List<GameObject>();
+	/// Max spawn distance from this BoidsController transform position.
+	public int spawnRadius;
+	/// Reference to in-scene goal GameObject for Boid objects in this BoidsController.
+	public GameObject goalObject;
 
-
-	/// Spawn the Boids
+	/// Spawn the starting Boids.
 	void SpawnInitialBoids(int numToSpawn, int spawnRadius)
 	{
 		Vector3 spawnOffset;
@@ -25,12 +24,25 @@ public class BoidsController : MonoBehaviour
 			spawnOffset = new Vector3(Random.Range(-spawnRadius, spawnRadius),
 										Random.Range(-spawnRadius, spawnRadius),
 										Random.Range(-spawnRadius, spawnRadius));
-			_allBoids.Add(Instantiate(_prefabBoid, transform.position + spawnOffset, Quaternion.identity));
+			GameObject boidObject = Instantiate(prefabBoid, transform.position + spawnOffset, Quaternion.identity);
+			allBoids.Add(boidObject);
+			Boid boid = boidObject.GetComponent<Boid>();
+			if (boid)
+			{
+				boid._parentBoidsController = this;
+			}
 		}
 	}
-
+	
 	void Awake()
 	{
-		SpawnInitialBoids(_numBoids, _spawnRadius);
+		SpawnInitialBoids(numBoids, spawnRadius);
+	}
+
+	/// Return the position the goal GameObject of this BoidsController.
+	/// @return - position of the goal GameObject
+	public Vector3 GetGoalPosition()
+	{
+		return goalObject.transform.position;
 	}
 }
